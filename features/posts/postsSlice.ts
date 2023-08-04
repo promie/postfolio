3308263fc7df320e64792f4d2e87b80953c5d50c
Promie Yutasane <promie.yutasane@gmail.com>
2023-08-04 14:19:59 +1000

    PF-06 constructed the postsActions and Slice to get posts

--- features/posts/postsSlice.ts
+++ features/posts/postsSlice.ts
@@ -1,23 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { getPosts } from './postsAction'
 
-const initialState = {
+const initialState: any = {
   loading: false,
-  error: false,
+  error: null,
   success: false,
-  posts: [
-    {
-      userId: 1,
-    },
-    {
-      userId: 2,
-    },
-  ],
+  posts: [],
 }
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {},
+  extraReducers: builder => {
+    // Get posts
+    builder.addCase(getPosts.pending, state => {
+      state.loading = true
+      state.error = null
+    })
+
+    builder.addCase(getPosts.fulfilled, (state, { payload }) => {
+      state.loading = false
+      state.posts = payload
+      state.success = true
+    })
+
+    builder.addCase(getPosts.rejected, (state, { payload }) => {
+      state.loading = false
+      state.posts = []
+      state.error = payload
+    })
+  },
 })
 
 export default postsSlice.reducer
