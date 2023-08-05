998a1e187b8bbb0e35159aef7082051e791714e3
Promie Yutasane <promie.yutasane@gmail.com>
2023-08-05 21:48:24 +1000

    PF-12 set global font to Nunito

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from 'next/app'
 import '@styles/globals.css'
+import { Nunito } from 'next/font/google'
 import { Provider } from 'react-redux'
 import store from '@store'
+
+const font = Nunito({ subsets: ['latin'] })
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <div className={font.className}>
+        <Component {...pageProps} />
+      </div>
     </Provider>
   )
 }
