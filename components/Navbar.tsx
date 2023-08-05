5e3d92c80591cdfa49816b5fd6c39effcf460522
Promie Yutasane <promie.yutasane@gmail.com>
2023-08-05 23:06:23 +1000

    PF-12 styled the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react'
+import Link from 'next/link'
 
 const Navbar: FC = () => {
   return (
     <nav className="h-[100px] flex items-center justify-center w-full bg-white z-10 shadow-sm mb-[20px] py-4 border-b-[1px]">
-      <div className="text-[30px] font-bold text-blue-500 bg-blue-500 p-[5px] text-white rounded-lg">
-        Postfolio
-      </div>
+      <Link href={'/'}>
+        <h1 className="text-[30px] font-bold text-blue-500 bg-blue-500 p-[5px] text-white rounded-lg cursor-pointer">
+          Postfolio
+        </h1>
+      </Link>
     </nav>
   )
 }
